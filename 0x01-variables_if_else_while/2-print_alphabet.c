0d82934de242a74ac96bab078e1e61c9a367b00e
Paul Nnaemeka <emekapaul302@gmail.com>
2023-05-21 16:25:46 +0100

    Solve task 2 of variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char sl = 'a';
+
+	while (sl <= 'z')
+	{
+		putchar(sl);
+		sl++;
+		putchar('\n');
+	}
+	return (0);
+}
