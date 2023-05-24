085d861472f0b2056dc13a738ffc8d75fe1f1606
Paul Nnaemeka <emekapaul302@gmail.com>
2023-05-24 02:01:44 +0100

    Solve task 1 of loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+void putcher(void)
+{
+	printf("_putchar\n");
+}
+
+int main(void)
+{
+	putcher();
+	return (0);
+}
