e6babc0cf4c25a4b85658b628e30008e092973cb
Paul Nnaemeka <emekapaul302@gmail.com>
2023-05-26 00:15:59 +0100

    Solve task7 of loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Main entry point.
+ * @num: The character to print.
+ *
+ * Return: Calculated output
+ */
+int print_last_digit(int num)
+{
+	int result = num % 10;
+
+	return (result);
+}
