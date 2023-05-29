5aa2cdd790bee42ab742c06b6acc1cb1adc5d0af
Paul Nnaemeka <emekapaul302@gmail.com>
2023-05-29 02:38:18 +0100

    Add conditional statements

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,34 @@
  */
 void times_table(void)
 {
-	int i;
+	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		int j;
-
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			int result = i * j;
-
-			if (result < 10)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar(result + '0');
+			_putchar(k + '0');
 			}
 
-			else
+
+			if (k < 10 && j != 0)
 			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
+
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
