03c5599d79c16888bd283f3d627a24505e90b799
Paul Nnaemeka <emekapaul302@gmail.com>
2023-05-30 03:30:34 +0100

    Solve task1 of pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - It  swaps the values of two integers.
+ * @a: The character to print
+ * @b: The character to print
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ int add(int n1, int n2);
 void print_to_98(int n);
 void print_times_table(int n);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
 #endif
