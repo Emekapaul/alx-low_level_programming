593e2bd78b24a344d1187a43e61e33d2ea8c1da0
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-01 05:10:07 +0100

    Solve task1 of more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -25,6 +25,10 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+/* More pointers task */
+
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
