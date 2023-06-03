c9f28467f1f9844e299c86acbb90bd7ef573c4b1
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-03 03:55:57 +0100

    Solve task6 of more functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_line - function that draws a straight line in the terminal.
+  * @n: The character to print.
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+
+		else
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 /* More functions and nested loops task */
 
+void print_line(int n);
 void more_numbers(void);
 void print_most_numbers(void);
 void print_numbers(void);
