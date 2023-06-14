5d9a876bb76ba24dd93bbddc3f3340e73f61dfa6
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-15 00:49:49 +0100

    Solve task2 of 0x02-functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - function that prints 10 times the alphabet,
+  *in lowercase.
+  */
+void print_alphabet_x10(void)
+{
+	int cl;
+	char lc;
+
+	for (cl = 1; cl <= 10; cl++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+		{
+			_putchar(lc);
+		}
+	_putchar('\n');
+	}
+
+	
+}
