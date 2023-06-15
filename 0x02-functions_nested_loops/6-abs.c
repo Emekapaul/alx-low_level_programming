e97cfb805b8e3a86412847638d52bf1f737171f4
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-15 05:06:19 +0100

    Solve task6 of 0x02-functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  *_abs - function that computes the absolute value
+  *of an integer.
+  *
+  *@n: The int to print.
+  *Return: Absolute value
+  */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
