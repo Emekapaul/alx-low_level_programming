3e96b11a5026cca8372789abc1062812f1724645
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-16 04:05:45 +0100

    Solve task9 of 0x02-functions

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  *times_table - function that prints the 9 times table,
+  *starting with 0.
+  */
+void times_table(void)
+{
+	int vt = 0;
+	int ht;
+
+	while (vt <= 9)
+	{
+		ht = 0;
+		while (ht <= 9)
+		{
+			if (ht == 9 && (vt * ht) / 10 + '0' == '0')
+			{
+				_putchar(' ');
+				_putchar((vt * ht) % 10 + '0');
+				ht++;
+			}
+
+			else if (ht == 9)
+			{
+				_putchar((vt * ht) / 10 + '0');
+				_putchar((vt * ht) % 10 + '0');
+				ht++;
+			}
+
+			else if ((vt * ht) / 10 + '0' != '0')
+			{
+				_putchar((vt * ht) / 10 + '0');
+				_putchar((vt * ht) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			ht++;
+			}
+
+			else
+			{
+				_putchar(' ');
+				_putchar((vt * ht) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+				ht++;
+			}
+		}
+	_putchar('\n');
+	vt++;
+	}
+}
