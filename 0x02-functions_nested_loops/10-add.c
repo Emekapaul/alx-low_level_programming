be8a46401b7e86d1a44980e4b9362643d92ee547
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-16 01:17:16 +0100

    Solve task10 of 0x02-functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  *add - function that adds two integers and returns the result.
+  *@n1: - The integer to print.
+  *@n2: - The integer to print.
+  *
+  *Return: n1 or n2.
+  */
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
