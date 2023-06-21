007e89f83414f5d1894b614049897e2327f72606
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-21 11:33:48 +0100

    increment by 2 during iteration in task6 of 0x05

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,19 +11,8 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
-
-		else if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
-
-		i++;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
