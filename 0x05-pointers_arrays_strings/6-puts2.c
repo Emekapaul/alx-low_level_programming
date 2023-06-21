d0c150c54068e9dd224a381cc87cf778217cd0a4
Paul Nnaemeka <emekapaul302@gmail.com>
2023-06-21 11:21:44 +0100

    Add if condition to task 6 0f 0x05-pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,13 @@ void puts2(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (i % 2 == 0)
+		if (i == 0)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+
+		else if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 			i++;
