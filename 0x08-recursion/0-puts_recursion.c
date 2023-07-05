64837418f18630105cfd937160b1838d7ac33d66
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-05 23:51:28 +0100

    Solve task 0 of 0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *_puts_recursion - function that prints a string.
+  *@s: The string to print.
+  */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+		_puts_recursion(s + i);
+	}
+	
+	else
+	{
+		_putchar('\n');
+	}
+}
