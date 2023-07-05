185242465e92222bacfe994be20ab10f2573f4a5
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-05 01:43:59 +0100

    Add '\0' for task 4 of 0x07-even more pointers

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,10 +11,10 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	while (s[i] != '0')
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (accept[j] >= '\0')
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
