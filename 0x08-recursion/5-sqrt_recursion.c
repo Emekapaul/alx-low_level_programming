e283b13883028c65d23f68e1c0ba43560e5624dd
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-07 05:23:03 +0100

    Solve task 5 of 0x08-recursions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int helper_sqrt_func(int num, int i);
+
+/**
+  *_sqrt_recursion - function that returns the natural square
+  *root of a number.
+  *@n: The integer to print.
+  *
+  *Return: int.
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper_sqrt_func(n, 0));
+}
+
+int helper_sqrt_func(int num, int j)
+{
+	if (j * j > num)
+		return (-1);
+	if (j * j == num)
+		return (j);
+	return (helper_sqrt_func(num, j + 1));
+}
