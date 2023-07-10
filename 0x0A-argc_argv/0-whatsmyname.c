fc3774ed74efdcafc48515b0f2b089798435062f
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-10 01:13:30 +0100

    Solve task 0 of 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point.
+  *@argv: Strings argument
+  *@argc: Number of entered arguments
+  *
+  *Return: Always 0 (Succes)
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
