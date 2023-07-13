31c29d42834feec63cdb2be225b5e58a242822e6
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-13 02:51:57 +0100

    Allocate the right memory task 3 0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,24 +27,25 @@ int **alloc_grid(int width, int height)
 		return (0);
 	}
 
-	for (i = 0; i < width; i++)
-	{
-		p[i] = (int *) malloc(height * sizeof(int));
+	p[0] = (int *) malloc(width * height * sizeof(int));
 
-		if (p[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(p[j]);
-			}
+	if (p[0] == NULL)
+	{
 		free(p);
 		return (0);
-		}
+	}
 
-		for (j = 0; j < height; j++)
-		{
-			*(*(p + i) + j) = 0;
-		}
+	for (i = 1; i < width; i++)
+	{
+		 p[i] = p[0] + i * height;
+	}
+
+	for (i = 0; i < width; i++)
+    	{
+        	for (j = 0; j < height; j++)
+        	{
+            		*(*(p + i) + j) = 0;
+        	}
 	}
 
 	return (p);
