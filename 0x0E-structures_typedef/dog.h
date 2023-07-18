258c9c8ea3197d7d28ab87fefc0eb848e82659bd
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-18 22:18:39 +0100

    Solve task 0 of 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - Dog data
+  *@name: Name of dog
+  *@age: Age of dog
+  *@owner: Owner of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
