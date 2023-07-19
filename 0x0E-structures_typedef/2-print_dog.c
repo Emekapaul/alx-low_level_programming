52b1382659cf021f7213928d86641abc5c4124b7
Paul Nnaemeka <emekapaul302@gmail.com>
2023-07-19 03:31:05 +0100

    Use -> for task 2 of 0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("(nil)");
+		d->name = "(nil)";
 	}
 
 	if (d->owner == NULL)
 	{
-		printf("(nil)");
+		d->owner = "(nil)";
 	}
 
 	printf("Name: %s\n", (*d).name);
