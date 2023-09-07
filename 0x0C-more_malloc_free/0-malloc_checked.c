920ce19257a0c41ed926f578ca25d7d4616e129c
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-07 21:13:03 +0100

    Solve task 0 of 0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *malloc_checked - Write a function that allocates memory using malloc.
+  *@b: The size of malloc.
+  *Return: pointer to the allocated memory
+  */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(sizeof(*ptr) * b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
