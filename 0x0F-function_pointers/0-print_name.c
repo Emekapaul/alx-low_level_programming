1fcb37f1e9f7a93bd4310be67ba624c49de43cfa
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-13 20:46:44 +0100

    Account for edge case task0 0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
   * print_name - Write a function that prints a name.
@@ -7,5 +8,9 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
