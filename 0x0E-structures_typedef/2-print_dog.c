ecfa56f2f46c0ae15a4c68ffe6aa503278501ea5
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-13 02:09:44 +0100

    correct task2 of 0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,15 @@ void print_dog(struct dog *d)
 		printf(" ");
 	}
 
+	if (d->name == NULL)
+	{
+		printf("(nil)");
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("(nil)");
+	}
+
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
