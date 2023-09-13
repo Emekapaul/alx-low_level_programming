26ba602e9752247685a26a0f8788d23b8388f126
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-13 01:47:17 +0100

    Solve task 2 of 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+#include "stdlib.h"
+
+/**
+  * print_dog - Write a function that prints a struct dog
+  * @d: The pointer to the struct
+  */
+void print_dog(struct dog *d)
+{
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
