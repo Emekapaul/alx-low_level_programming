d2b20f2e688f56f2d609fa2f28d1b4e23b0f5d58
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-13 02:14:09 +0100

    Use (nil) for task 2 of 0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,17 +10,17 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		printf(" ");
+		return;
 	}
 
 	if (d->name == NULL)
 	{
-		printf("(nil)");
+		d->name = "(nil)";
 	}
 
 	if (d->owner == NULL)
 	{
-		printf("(nil)");
+		d->owner = "(nil)";
 	}
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
