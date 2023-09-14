d332904126fc6b6a63e451535b66c12160aca3a4
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-14 04:58:41 +0100

    Account for edge case task1 0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (action == NULL)
+	{
+		return;
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
