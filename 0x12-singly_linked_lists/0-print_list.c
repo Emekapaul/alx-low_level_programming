1cbad32821a685e15c4da25640dbc301192942b6
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-22 05:40:06 +0100

    Solve task 0 of 0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_list - Write a function that prints all the elements of a list_t
+  * list.
+  * @h: The head to the first list
+  *
+  *Return: size_t
+  */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+	const list_t *ptr = NULL;
+
+	ptr = h;
+
+	for (; ptr != NULL; count++)
+	{
+		if(ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+
+
+		else
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+
+		ptr = ptr->next;
+	}
+
+	 return (count);
+}
