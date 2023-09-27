80e105266bc6a6b2d104a32fb0ee557b8fd53160
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-27 23:56:31 +0100

    Solve task 0 of 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_listint - Write a function that prints all the elements of a
+  * listint_t list.
+  * @h: POinter to the first node.
+  */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count;
+	const listint_t *ptr = NULL;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	ptr = h;
+
+	for (count = 0; ptr != NULL; count++)
+	{
+		printf("%d\n", ptr->n);
+
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
