6f1de5c9a1ffe1095e671ceea4b2cc0de898d986
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-28 00:00:36 +0100

    Use betty style task0 0x13-more_singly_linkedlists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,6 +5,7 @@
   * print_listint - Write a function that prints all the elements of a
   * listint_t list.
   * @h: POinter to the first node.
+  * Return: the number of nodes
   */
 size_t print_listint(const listint_t *h)
 {
