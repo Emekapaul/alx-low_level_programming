7461335f3e6885e5f709bfbb55a621c81d0878e1
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-28 00:10:00 +0100

    Solve task 1 of 0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
   * print_listint - Write a function that prints all the elements of a
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * listint_len - Write a function that returns the number of elements in
+  * a linked listint_t list.
+  * @h: Pointer to first node.
+  *
+  *Return: Number of elements
+  */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count;
+	const listint_t *ptr = NULL;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	ptr = h;
+
+	for (count = 0; ptr != NULL; count++)
+	{
+		ptr = ptr->next;
+	}
+
+	return count;
+}
