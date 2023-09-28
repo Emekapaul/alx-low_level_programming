3e2963e153d537bcf8d7b6d59ddbe6aa2d700a87
Paul Nnaemeka <emekapaul302@gmail.com>
2023-09-28 03:00:35 +0100

    Solve task 5 of 0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * free_listint2 - Write a function that frees a listint_t list.
+  * @head: The pointer to the node to free.
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
+	*head = NULL;
+}
