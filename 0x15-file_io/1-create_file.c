4421b67706496cf2cae177b442e285474099a4c2
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 03:59:14 +0100

    Initialize len outside task 1 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,9 +26,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	len = 0;
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (; text_content[len]; len++)
 		{
 		}
 	}
@@ -37,6 +38,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (_write == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 
