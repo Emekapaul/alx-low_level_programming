467a9911e853c3e7729c811ee1c33ebf4d9330ed
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 03:49:48 +0100

    Take fd above 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,17 +20,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
 		{
 		}
-	}
-
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-	if (fd == -1)
-	{
-		return (-1);
 	}
 
 	_write = write(fd, text_content, len);
