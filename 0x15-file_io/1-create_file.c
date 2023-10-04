69ecafaf038f6a7153ee3d3f194f43b4b20e8cff
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 03:02:08 +0100

    Use for loop task 1 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len = strlen(text_content);
+	int len;
 	ssize_t _write;
 
+	for (len = 0; text_content[len] != '\0'; len++)
+	{
+	}
 	if (filename == NULL)
 	{
 		return (-1);
