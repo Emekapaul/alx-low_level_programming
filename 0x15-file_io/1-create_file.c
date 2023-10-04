853d121497ed841e126f03281e74c78a0646c920
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 03:33:24 +0100

    move loop below task 1 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,16 +15,16 @@ int create_file(const char *filename, char *text_content)
 	int len;
 	ssize_t _write;
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
 		{
 		}
-	}
-
-	if (filename == NULL)
-	{
-		return (-1);
 	}
 
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
