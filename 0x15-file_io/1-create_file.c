7d7cc5aa6404c0054fcadd176cfa1920c2f529c9
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 02:43:22 +0100

    Solve task 1 of 0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string.h>
+/**
+  * create_file - Create a function that creates a file.
+  * @filename: he name of the file to create.
+  * @text_content: is a NULL terminated string to write to the file.
+  *
+  * Return: Returns: 1 on success, -1 on failure (file can not be created,
+  * file can not be written, write “fails”, etc…),
+  * if filename is NULL return -1.
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int len = strlen(text_content);
+	ssize_t _write;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+		return (fd);
+	}
+
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	_write = write(fd, text_content, len);
+
+	if (_write == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
