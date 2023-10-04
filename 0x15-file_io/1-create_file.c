7e32d21fbb28f513a58865d497dc070d71aa7830
Paul Nnaemeka <emekapaul302@gmail.com>
2023-10-04 03:23:19 +0100

    Use if condition task 1 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,13 @@ int create_file(const char *filename, char *text_content)
 	int len;
 	ssize_t _write;
 
-	for (len = 0; text_content[len] != '\0'; len++)
+	if (text_content != NULL)
 	{
+		for (len = 0; text_content[len] != '\0'; len++)
+		{
+		}
 	}
+
 	if (filename == NULL)
 	{
 		return (-1);
