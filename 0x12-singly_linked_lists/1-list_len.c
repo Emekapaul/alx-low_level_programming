c546edcaed297b4db511fd3fb9bc5f0365d35a9f
Paul Nnaemeka <emekapaul302@gmail.com>
2023-11-12 06:12:41 +0100

    Resolve task 1 of 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,21 +3,26 @@
 /**
   * list_len - Write a function that returns the number of elements in a
   * linked list_t list.
-  * @h: Header to the first element
-  * Return: size_t.
+  * @h: Head pointer
+  *
+  * Return: the number of elements in a linked list_t list.
   */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t no_nodes = 0;
 
 	const list_t *ptr = NULL;
 
+	if (!h)
+		return (0);
+
 	ptr = h;
 
-	for (; ptr != NULL; count++)
+	while (ptr)
 	{
+		no_nodes++;
 		ptr = ptr->next;
 	}
 
-	return (count);
+	return (no_nodes);
 }
